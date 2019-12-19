94ef96361b1d21dae01a9577eeb03f295deafeb3
LusioF <lusiof@gmail.com>
2019-12-18 20:49:43 -0500

    created function to get day from array and create new date and return

--- js/site.js
+++ js/site.js
@@ -3,3 +3,17 @@ unction formatDate(string) {
   var formattedDay = date.toString();
   return formattedDay;
 }
+
+function getDay(string) {
+  var weekday = new Array(7);
+  weekday[0] = "Sun";
+  weekday[1] = "Mon";
+  weekday[2] = "Tue";
+  weekday[3] = "Wed";
+  weekday[4] = "Thu";
+  weekday[5] = "Fri";
+  weekday[6] = "Sat";
+  var date = new Date(string);
+  var day = date.getDay();
+  return weekday[day];
+}
